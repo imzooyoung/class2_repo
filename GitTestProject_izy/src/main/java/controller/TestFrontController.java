46e05405878305308aab48bc04491d520a05530c
user <user@DESKTOP-2PGRMF4>
2022-08-10 12:13:06 +0900

    Modify
    - index.jsp 주석
    TestFrontController doprocess 정의

--- GitTestProject_izy/src/main/java/controller/TestFrontController.java
+++ GitTestProject_izy/src/main/java/controller/TestFrontController.java
@@ -12,29 +12,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("*.test")
 public class TestFrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestFrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// doGet(), doPost() -> doProcess() 호출 코드 생성
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
